d4fbf311a16f5cb23312d0297a7c86b5893891e6
smazzanti <41154179+smazzanti@users.noreply.github.com>
2023-03-05 20:51:35 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='valicast',
+    version='0.1',
+    description='Validation method for forecasts',
+    url='https://github.com/smazzanti/valicast',
+    author='Samuele Mazzanti',
+    author_email='mazzanti.sam@gmail.com',
+    license='MIT',
+    packages=['valicast'],
+    install_requires=[
+        'numpy>=1.18.1'
+    ],
+    zip_safe=False
+)
