92a5f938f8a16c3f182018ea96c5a513e6f0fc08
smazzanti <41154179+smazzanti@users.noreply.github.com>
2023-03-05 21:03:36 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     author='Samuele Mazzanti',
     author_email='mazzanti.sam@gmail.com',
     license='MIT',
-    packages=['src'],
+    packages=['valicast'],
     install_requires=[
         'numpy>=1.18.1'
     ],
